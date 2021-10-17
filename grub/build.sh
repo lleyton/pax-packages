4a755184647843764c8ca502c549bd01b8c12ef7
infinitebash <32340104+infinitebash@users.noreply.github.com>
2021-10-16 23:57:07 -0700

    add grub package

---
+++ grub/build.sh
@@ -0,0 +1,18 @@
+VER="2.06"
+SRC="https://ftp.gnu.org/gnu/grub/grub-$VER.tar.xz"
+
+curl -L $SRC -o src.tar.gz
+tar -xJf src.tar.gz
+mv grub-* src
+
+mkdir -p pkg/usr pkg/etc build
+(cd build &&
+../src/configure --prefix=$(pwd)/../pkg/usr \
+            --sysconfdir=$(pwd)/../pkg/etc \
+            --disable-efiemu \
+            --disable-werror
+make -j$(nproc) &&
+make install)
+cp package.toml pkg
+
+( cd pkg && tar cJf ../../out/grub.apkg * )
