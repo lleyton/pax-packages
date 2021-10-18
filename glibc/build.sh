034c2934a0e07968e6cc7efd9255bceb7f17e8d5
infinitebash <32340104+infinitebash@users.noreply.github.com>
2021-10-17 21:33:38 -0700

    fix glibc build script make install into the pkg/usr directory, not the system /usr

--- glibc/build.sh
+++ glibc/build.sh
@@ -5,7 +5,17 @@ tar -xJf src.tar.xz
 mv glibc-* src
 
 mkdir -p pkg/usr build
-(cd build && ../src/configure --prefix=$(pwd)/../pkg/usr --disable-werror --enable-kernel=3.2 --enable-stack-protector=strong --with-headers=/usr/include libc_cv_slibdir=/usr/lib && make && make install)
+(cd build &&
+../src/configure --prefix=$(pwd)/../pkg/usr \
+                 --disable-werror \
+                 --enable-kernel=3.2 \
+                 --enable-stack-protector=strong \
+                 --with-headers=/usr/include \
+                 --build=$(../src/scripts/config.guess) \
+                 --host="x86_64-pax-linux-gnu"
+                 libc_cv_slibdir=$(pwd)/../usr/lib && 
+make -j$(nproc) && 
+make install)
 cp package.toml pkg
 
 (cd pkg && tar cfJ ../../out/glibc.apkg *)
