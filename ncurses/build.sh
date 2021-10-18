fd6947df018de6e63b800a02e3eed24b03026fd3
infinitebash <32340104+infinitebash@users.noreply.github.com>
2021-10-17 22:15:33 -0700

    add ncurses

---
+++ ncurses/build.sh
@@ -0,0 +1,22 @@
+SRC="https://invisible-island.net/datafiles/release/ncurses.tar.gz"
+
+curl -L $SRC -o src.tar.gz
+tar -xzf src.tar.gz
+mv ncurses-* src
+
+mkdir -p pkg/usr build
+(cd build &&
+../src/configure --prefix=$(pwd)/../pkg/usr \
+                 --build=$(../src/config.guess) \
+                 --mandir=$(pwd)/../pkg/usr/share/man \
+                 --with-manpage-format=normal \
+                 --with-shared \
+                 --without-debug \
+                 --without-ada \
+                 --without-normal \
+                 --enable-widec && 
+make -j$(nproc) && 
+make install)
+cp package.toml pkg
+
+(cd pkg && tar cfJ ../../out/ncurses.apkg *)
