b8e3dbfc9fdfe55b610481f4e7159db69c85b56c
Lleyton Gray <lleyton@innatical.com>
2021-11-22 11:04:54 -0800

    zstd

---
+++ zstd/build.sh
@@ -0,0 +1,19 @@
+VER="1.5.0"
+SRC="https://github.com/facebook/zstd/releases/download/v$VER/zstd-$VER.tar.gz"
+
+curl -L $SRC -o src.tar.xz
+tar -xJf src.tar.xz
+mv zstd-* src
+
+mkdir -p pkg/usr
+(cd src &&
+make &&
+make check &&
+make prefix=$(pwd)/../pkg install &&
+rm -v ../pkg/usr/lib/libzstd.a)
+cp package.toml pkg
+
+( cd pkg && tar --zstd -cf ../../out/zstd.apkg * )
+
+# Cleanup
+rm -rf pkg src src.tar.xz
