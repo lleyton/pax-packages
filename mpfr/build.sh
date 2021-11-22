e2d8f7bbce60065910dd3ed4c2ca37336dfb95d1
Jamie Thalacker <hello@jamiethalacker.dev>
2021-11-22 00:42:36 -0800

    :sparkles: I'd just like to interject for a moment.
    What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.

---
+++ mpfr/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+VER="4.1.0"
+SRC="https://www.mpfr.org/mpfr-$VER/mpfr-$VER.tar.xz"
+
+curl -L $SRC -o src.tar.xz
+tar -xJf src.tar.xz
+mv mpfr-* src
+
+mkdir -p pkg
+(cd src                                         &&
+ ./configure --prefix=/usr                      \
+             --enable-thread-safe               \
+             --disable-static                   \
+             --docdir=/usr/share/doc/mpfr-$VER  \
+             --host="x86_64-pax-linux-gnu"      &&
+make                                            &&
+make html                                       &&
+make check                                      &&
+make DESTDIR=$(pwd)/../pkg install              &&
+make DESTDIR=$(pwd)/../pkg install html)
+
+cp preremove.sh pkg
+cp postinstall.sh pkg
+cp package.toml pkg
+( cd pkg && tar --zstd -cf ../../out/mpfr.apkg * )
+
+# Cleanup
+
+rm -r pkg
+rm -r src
+rm src.tar.xz
