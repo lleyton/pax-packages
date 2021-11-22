2d93803217eaa3a048a18d0d542b7bb892ac5ab6
Lleyton Gray <lleyton@innatical.com>
2021-11-21 22:01:57 -0800

    gawk

---
+++ gawk/build.sh
@@ -0,0 +1,26 @@
+VER="5.1.0"
+SRC="https://ftp.gnu.org/gnu/gawk/gawk-$VER.tar.xz"
+
+curl -L $SRC -o src.tar.xz
+tar -xJf src.tar.xz
+mv gawk-* src
+
+mkdir -p pkg
+(cd src                                                      &&
+./configure --prefix=/usr   \
+            --host="x86_64-pax-linux-gnu" \
+            --build=$(./config.guess)                        &&
+make                                                         &&
+make DESTDIR=$(pwd)/../pkg install)
+
+cp postremove.sh pkg
+cp postinstall.sh pkg
+cp package.toml pkg
+( cd pkg && tar --zstd -cf ../../out/gawk.apkg * )
+
+# Cleanup
+
+rm -r pkg
+rm -r src
+rm -r build
+rm src.tar.xz
